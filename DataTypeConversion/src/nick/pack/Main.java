951ce775e40de4f4a8de45c1e882a386960829b9
Andreynikonov23 <76613379+Andreynikonov23@users.noreply.github.com>
2022-02-07 09:56:09 +0300

    Add files via upload

---
+++ DataTypeConversion/src/nick/pack/Main.java
@@ -0,0 +1,64 @@
+package nick.pack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Задание №1: " + task1());
+        System.out.println("Задание №2: " + task2());
+        System.out.println("Задание №3: " + task3());
+        System.out.println("Задание №4: " + task4());
+        System.out.println("Задание №5: " + task5());
+    }
+    public static long task1(){
+        /*Расставьте правильно операторы приведения типа,
+        чтобы получился ответ: d > 0
+         */
+        int a = 0;
+        int b = a + 46;
+        byte c = (byte) (a*b);
+        double f = 1234.15;
+        long d =(long) (a + f / c + b);
+        return d;
+    }
+    public static double task2(){
+        /*
+        Расставьте правильно операторы приведения типа,
+        чтобы получился ответ: d = 3.765
+         */
+        int a = 15;
+        int b = 4;
+        float c = (float) a / b;
+        double d = a * 1e-3 + c;
+        return d;
+    }
+    public static int task3(){
+        /*
+        Добавить одну операцию по преобразованию типа,
+        чтобы получался ответ: b = 0
+         */
+        float f = (float)128.50;
+        int i = (int)f;
+        int b = (byte)(i + f);
+        return b;
+    }
+    public static char task4(){
+        /*
+        Добавить одну операцию по преобразованию типа,
+        чтобы получался ответ: nine = 9
+         */
+        short number = 9;
+        char zero = '0';
+        int nine = (zero + number);
+        return (char) nine;
+    }
+    public static short task5(){
+        /*
+        Расставьте правильно операторы приведения типа,
+        чтобы получился ответ: c = 256
+         */
+        int a = (byte)44;
+        int b = 300;
+        short c = (short) (b - a);
+        return c;
+    }
+}
